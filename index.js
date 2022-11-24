063b16abca5ed9033a0006c47fa10e3be40c204d
koshun-code <sit.com.k@gmail.com>
2022-11-24 22:43:45 +0300

    add linter to code

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-
-import _ from 'lodash';
-import { sortByFoldersName, getPath, move } from './src/worker.js';
 import fs from 'fs';
 import fsp from 'fs/promises';
+import { sortByFoldersName, getPath, move } from './src/worker.js';
 
 export default async (from) => {
   const sortedFiles = await sortByFoldersName(from);
-  // console.log(sortedFiles);
-  // return;
-  const res = Object.keys(sortedFiles).map( async (folder) => {
+  const res = Object.keys(sortedFiles).map(async (folder) => {
     const pathsFolder = getPath(`${from}/${folder}`);
     if (!fs.existsSync(pathsFolder)) {
-      await fsp.mkdir(pathsFolder)
+      await fsp.mkdir(pathsFolder);
     }
     await move(from, `${from}/${folder}`, sortedFiles[folder]);
   });
--- src/worker.js
+++ src/worker.js
@@ -1,41 +1,41 @@
 import fs from 'fs';
 import path from 'path';
 import _ from 'lodash';
+
 const { promises: fsp } = fs;
 
+export const getPath = (files) => path.resolve(...files.split('/'));
 
 export const move = async (src, dest, data = []) => {
   const fromFolder = getPath(src);
   const toFolder = getPath(dest);
   await data.map(async (item) => {
-    await fsp.copyFile(`${fromFolder}/${item}`,`${toFolder}/${item}`);
+    await fsp.copyFile(`${fromFolder}/${item}`, `${toFolder}/${item}`);
     await fsp.unlink(`${fromFolder}/${item}`)
-    .then(() => console.log('Success moved'))
-    .catch((error) => console.log(error));
+      .then(() => console.log('Success moved'))
+      .catch((error) => console.log(error));
   });
 };
 
-export const getPath = (files) => path.resolve(...files.split('/'));
-
 export const getNormalizeNames = async (pathToFolder) => {
   const files = await fsp.readdir(pathToFolder);
-   return  files.map( (file) => {
-    const pattern = new RegExp('-|_', 'gim');
+  return files.map((file) => {
+    const pattern = /-| /gim; // new RegExp('-|_', 'gim');
     const nFile = file.toLowerCase().replaceAll(pattern, ' ');
     fs.renameSync(getPath(`${pathToFolder}/${file}`), getPath(`${pathToFolder}/${nFile}`))
     return nFile;
-   });
+  });
 };
 
-export const isDir = (path) => {
-  const stat =  fs.statSync(path);
+export const isDir = (pathToDir) => {
+  const stat = fs.statSync(pathToDir);
   return stat.isDirectory();
 };
 
 export const getWordsFromFilename = (fileName) => {
   const cuteExt = fileName.split('.')[0];
   return _.words(cuteExt);
-}
+};
 
 const getMap = () => {
   const pathToMap = getPath('map.json');
@@ -45,10 +45,9 @@ const getMap = () => {
 
 export const sortByFoldersName = async (pathToFolder) => {
   console.log(getPath('map.json'));
-  //return;
-  const map = getMap()
+  const map = getMap();
   const normFiles = await getNormalizeNames(pathToFolder); 
-  const files =  normFiles.filter((file) => !isDir(getPath(`${pathToFolder}/${file}`)));
+  const files = normFiles.filter((file) => !isDir(getPath(`${pathToFolder}/${file}`)));
   return Object.keys(map).reduce((acc, key) => {
     acc[key] = files.filter((file) => _.intersectionWith(getWordsFromFilename(file), _.get(map, key), (a, b) => {
       const regExp = new RegExp(`${b}`);
